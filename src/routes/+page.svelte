5a48e341b709e1db7357efbc4514ce2bc8eb3ab8
Arvind <144233717+arvind-mangalarapu@users.noreply.github.com>
2024-09-05 15:31:15 +0530

    changed to here

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-    import GradientButton from '../lib/components/GradientButton.svelte';
-  </script>
-      <main>
-          <h1 class="text-[110px] text-center bg-gray-100">HELLO WORLD</h1>
-            <div class="flex items-center justify-center h-screen bg-gray-100">
-              
-              <GradientButton />
-              <div>click hear <GradientButton /></div>
-            </div>
-           
-      </main>
-  
+	import GradientButton from '../lib/components/GradientButton.svelte';
+</script>
+
+<main>
+	<h1 class="text-[110px] text-center bg-gray-100">HELLO WORLD</h1>
+	<div class="flex items-center justify-center h-screen bg-gray-100">
+		<GradientButton />
+		<div>click here <GradientButton /></div>
+	</div>
+</main>
